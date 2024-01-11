a3e6f921318787d8771b97f152bbd60a8203c036
Brad McAlister <brad.mcalister@zelis.com>
2024-01-10 18:15:32 -0600

    fix article

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,18 +1,29 @@
+import type { Post } from 'types/graphql'
+
 import { Link, routes } from '@redwoodjs/router'
 
-const Article = ({ article }) => {
+const truncate = (text: string, length: number) => {
+  return text.substring(0, length) + '...'
+}
+
+interface Props {
+  article: Omit<Post, 'createdAt'>
+  summary?: boolean
+}
+
+const Article = ({ article, summary = false }: Props) => {
   return (
     <article>
       <header>
-        <h2 className="text-xl text-blue-700 font-semibold">
+        <h2 className="text-xl font-semibold text-blue-700">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
-          <span className="ml-2 text-gray-400 font-normal">
-            by {article.user.name}
+          <span className="ml-2 font-normal text-gray-400">
+            {summary ? truncate(article.body, 100) : article.body}
           </span>
         </h2>
       </header>
 
-      <div className="mt-2 text-gray-900 font-light">{article.body}</div>
+      <div className="mt-2 font-light text-gray-900">{article.body}</div>
     </article>
   )
 }
